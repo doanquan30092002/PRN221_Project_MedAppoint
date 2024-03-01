ada13abce3647dd76040a7403a59e3f53dfc402f
dattuanh <hdat1216@gmail.com>
2024-03-02 02:04:25 +0700

    add manage service

--- PRN221_Project_MedAppoint/Areas/Admin/Pages/ManageService/Index.cshtml.cs
+++ PRN221_Project_MedAppoint/Areas/Admin/Pages/ManageService/Index.cshtml.cs
@@ -1,22 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using PRN221_Project_MedAppoint.Model;
-using System.Text;
-using System.Text.Json;
 
-namespace PRN221_Project_MedAppoint.Areas.User.Pages.Customer
+namespace PRN221_Project_MedAppoint.Areas.Admin.Pages.ManageService
 {
-    public class BookDoctorModel : PageModel
+    public class IndexModel : PageModel
     {
         private readonly PRN221_Project_MedAppoint.Model.MyMedDbContext _context;
 
-        public BookDoctorModel(MyMedDbContext context)
+        public IndexModel(PRN221_Project_MedAppoint.Model.MyMedDbContext context)
         {
             _context = context;
         }
 
-        public IList<Users> Users { get; set; } = default!;
+        public IList<Services> Services { get;set; } = default!;
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -26,24 +30,21 @@ namespace PRN221_Project_MedAppoint.Areas.User.Pages.Customer
                 string serializedUser = Encoding.UTF8.GetString(userBytes);
                 Users u = JsonSerializer.Deserialize<Users>(serializedUser);
                 ViewData["user"] = u;
-                if (u.RoleID == 2)
+                if (u.RoleID == 1)
                 {
-                    Users = await _context.Users
-                    .Include(u => u.Role).ToListAsync();
+                    Services = await _context.Services.ToListAsync();
                     return Page();
                 }
                 else
                 {
                     return RedirectToPage("/Error", new { area = "User" });
                 }
+
             }
             else
             {
-                return RedirectToPage("/Login", new { area = "User" });
+                return RedirectToPage("/Index", new { area = "Admin" });
             }
         }
-
-
-
     }
 }
