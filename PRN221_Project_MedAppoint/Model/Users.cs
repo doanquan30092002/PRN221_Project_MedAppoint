c4edfd21b42b7e0da3328b596d72b0dae65a174d
dattuanh <hdat1216@gmail.com>
2024-03-02 03:15:26 +0700

    add price in user table

--- PRN221_Project_MedAppoint/Model/Users.cs
+++ PRN221_Project_MedAppoint/Model/Users.cs
@@ -8,24 +8,34 @@ namespace PRN221_Project_MedAppoint.Model
     {
         [Key]
         public int UserID { get; set; }
+
         [Column(TypeName = "NVARCHAR")]
         [StringLength(255)]
         public string? Username { get; set; }
+
         [Column(TypeName = "VARCHAR")]
         [StringLength(255,MinimumLength =6)]
         public string? Password { get; set; }
+
         [EmailAddress]
         public string? Email { get; set; }
+
         [Phone]
         public string? Phone { get; set; }
+
         [Column(TypeName = "NVARCHAR")]
         [StringLength(255)]
         public string? Address { get; set; }
+
         public int? RoleID { get; set; }
+
         [Column(TypeName = "VARCHAR")]
         [StringLength(255)]
         public string? Avatar { get; set; }
+
         public bool? Gender { get; set; }
+        
+        public decimal? DoctorPrice { get; set; }
 
         public Roles Role { get; set; }
         public bool IsDeleted { get; set; } = false;
